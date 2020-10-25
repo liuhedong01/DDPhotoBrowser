6bab51e0caabedec0176a4dbb039450f0c55614d
lhd <liuhedong01@163.com>
2020-10-25 17:02:33 +0800

    提交改变

---
+++ DDPhotoBrowser/Classes/DDGetImageViewEngine/DDGetImageViewEngine.h
@@ -0,0 +1,19 @@
+//
+//  DDGetImageViewEngine.h
+//  DDPhotoBrowser
+//
+//  Created by 刘和东 on 2015/5/21.
+//  Copyright © 2015年 刘和东. All rights reserved.
+//
+#import <UIKit/UIKit.h>
+#import <Foundation/Foundation.h>
+
+@protocol DDGetImageViewEngine <NSObject,NSCopying>
+
+- (UIImageView *)getImageViewWithFrame:(CGRect)frame;
+- (UIImageView *)getImageView;
+- (NSData *)getImageData;
+- (UIImage *)getImage;
+- (id)copyWithZone:(NSZone *)zone;
+
+@end
